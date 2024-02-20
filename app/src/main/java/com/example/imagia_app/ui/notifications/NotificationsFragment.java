451a59620ea49e3241700810b074f528ef4e0a6f
joelberzalgithub <120808626+joelberzalgithub@users.noreply.github.com>
2024-02-20 16:29:51 +0100

    Creada estructura pel registre d'usuaris

--- app/src/main/java/com/example/imagia_app/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/imagia_app/ui/notifications/NotificationsFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,18 +15,32 @@ import androidx.fragment.app.Fragment;
 import com.example.imagia_app.R;
 
 public class NotificationsFragment extends Fragment {
-    private Button registerButton;
+    private EditText editTextEmail;
     private EditText editTextNom;
     private EditText editTextTfn;
+    private EditText editTextCodi;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_notifications, container, false);
 
-        registerButton = view.findViewById(R.id.registerButton);
+        editTextEmail = view.findViewById(R.id.editTextEmail);
         editTextNom = view.findViewById(R.id.editTextNom);
         editTextTfn = view.findViewById(R.id.editTextTfn);
+        editTextCodi = view.findViewById(R.id.editTextCodi);
+        Button registerButton = view.findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(v -> {
+            if (editTextEmail.getText().toString().trim().isEmpty() ||
+                    editTextNom.getText().toString().trim().isEmpty() ||
+                    editTextTfn.getText().toString().trim().isEmpty() ||
+                    editTextCodi.getText().toString().trim().isEmpty()) {
+                Toast.makeText(requireContext(), "No t'has pogut registrar! Et falta inserir dades de l'usuari.", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(requireContext(), "T'has registrat amb èxit!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return view;
     }
